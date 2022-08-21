1edb0dd64530ad8955d5104274d709895870f071
Utkan <utkanocal@gmail.com>
2022-08-21 12:48:55 +0300

    card design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,9 @@
-import { Container, Box } from "@mui/material";
+import { Container, Box, Button, TextField, Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import {
-  useFirebaseListener,
-  deleteFirebaseDoc,
-  addFirebaseDoc,
-} from "../../firebase/firebaseConfigs";
+import { useFirebaseListener } from "../../firebase/firebaseConfigs";
 import { useDispatch } from "react-redux";
 import { deleteFBDoc, addFBDoc } from "../../redux/features/productsSlice";
 import SelectOption from "./Select";
-import { Button, TextField } from "@mui/material";
 import Posts from "../Posts/Posts";
 const Home = () => {
   const [newPostTitle, setNewPostTitle] = useState("");
@@ -22,7 +17,7 @@ const Home = () => {
   };
 
   const addNewPost = () => {
-  
+    if (newPostTitle !== "" && newPostDesc !== "" && categoryName.length >= 1) {
       dispatch(
         addFBDoc({
           title: newPostTitle,
@@ -33,8 +28,7 @@ const Home = () => {
       setCategoryName([]);
       setNewPostTitle("");
       setNewPostDesc("");
-
-
+    }
   };
 
   useEffect(() => {
@@ -43,11 +37,11 @@ const Home = () => {
     console.log(newPostDesc);
   }, [newPostDesc]);
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="lg">
       <Box component="form" sx={{ mt: 8 }}>
         <Box display={"flex"} alignContent="center" justifyContent={"center"}>
           <TextField
-            sx={{ minWidth: "300px" }}
+            // sx={{ minWidth: "300px" }}
             label="Add new post title..."
             margin="normal"
             required
@@ -60,40 +54,37 @@ const Home = () => {
             setCategoryName={setCategoryName}
           />
         </Box>
-       <Box display={"flex"} alignContent="center" justifyContent={"center"} flexDirection="column">
-       <TextField
+        <TextField
           label={`Add new post content... Max ${
             140 - newPostDesc.length
           } Characters.`}
           fullWidth
+          required
           multiline
           rows={4}
           value={newPostDesc}
           error={newPostDesc.length >= 140}
-         
           onChange={(e) => setNewPostDesc(e.target.value)}
         />
-         <Button
+
+        <Button
           variant="contained"
-          sx={{ mt: 2}}
+          fullWidth
+          sx={{ mt: 2 }}
           onClick={addNewPost}
           disabled={newPostDesc.length >= 140}
         >
-          {newPostDesc.length >= 140 ? "Max Limit 140 Characters!" :  "Add new post"}
+          {newPostDesc.length >= 140
+            ? "Max Limit 140 Characters!"
+            : "Add new post"}
         </Button>
-       </Box>
-       
       </Box>
 
-      <Box sx={{ mt: 8 }}>
+      <div className="grid">
         {products?.map((post) => (
           <Posts key={post.id} removeDoc={removeDoc} {...post} />
-          // <h2 >
-          //   {pro?.name}{pro?.category}
-          //   <button onClick={() => (pro.id)}>del</button>
-          // </h2>
         ))}
-      </Box>
+      </div>
     </Container>
   );
 };
